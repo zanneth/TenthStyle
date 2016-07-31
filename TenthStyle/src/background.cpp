bbd70b08c0432ae9b332ffac055250c48fd17f26
Charles Magahern <charles@magahern.com>
2016-07-31 02:11:24 -0700

    Fix shitty background animation

--- TenthStyle/src/background.cpp
+++ TenthStyle/src/background.cpp
@@ -22,30 +22,27 @@ TenthStyleBackground::TenthStyleBackground(zge::ZRenderContextRef context) :
 void TenthStyleBackground::_setup_camera()
 {
     ZRect viewport = _context->get_viewport();
-    float aspect = viewport.size.width / viewport.size.height;
-    
-    ZOrthoCameraRef camera = std::make_shared<ZOrthoCamera>();
-    camera->set_clipping_rect({0.0, 0.0, aspect, 1.0});
+    ZOrthoCameraRef camera = ZOrthoCamera::create();
+    camera->set_clipping_rect({0.0, 0.0, viewport.size.width, viewport.size.height});
     set_camera(camera);
 }
 
 void TenthStyleBackground::_setup_background()
 {
     const ZResourceBundle *resbundle = ZResourceBundle::get_main_bundle();
-    ZImageRef bgimg = std::make_shared<ZImage>(resbundle->get_path_for_resource("scrolling_bg.png"));
+    ZImageRef bgimg = ZImage::create(resbundle->get_path_for_resource("scrolling_bg.png"));
     ZSize2D bgimg_size = bgimg->get_size();
-    float bgimg_aspect = bgimg_size.width / bgimg_size.height;
     ZTextureRef bgtexture = _context->create_texture(bgimg);
     
-    ZSpriteNodeRef bg_node = std::make_shared<ZSpriteNode>();
+    ZSpriteNodeRef bg_node = ZSpriteNode::create();
     bg_node->set_texture(bgtexture);
-    bg_node->set_size({bgimg_aspect * 2.0f, 1.0});
+    bg_node->set_size({bgimg_size.width * 2.0f, bgimg_size.height});
     bg_node->set_position(ZVector::zero);
     bg_node->set_texture_rect({{0.0, 0.0}, {2.0, 1.0}});
     add_child(bg_node);
     
-    ZVector anim_end = {-bgimg_aspect, 0.0};
-    ZTranslationActionRef anim_action = std::make_shared<ZTranslationAction>(ZVector::zero, anim_end);
+    ZVector anim_end = {-bgimg_size.width, 0.0};
+    ZTranslationActionRef anim_action = ZTranslationAction::create(ZVector::zero, anim_end);
     anim_action->set_repeat_count(ZAction::repeat_count_infinity);
     anim_action->set_duration(ZTimeInterval(20.0));
     bg_node->add_action(anim_action);
--- TenthStyle/src/ship.cpp
+++ TenthStyle/src/ship.cpp
@@ -25,7 +25,7 @@ TenthStyleShip::TenthStyleShip(zge::ZRenderContextRef context) :
 
 void TenthStyleShip::_setup_camera()
 {
-    Z3DCameraRef camera = std::make_shared<Z3DCamera>();
+    Z3DCameraRef camera = Z3DCamera::create();
     camera->set_position({-0.5, 0.0, 0.0});
     camera->set_look({-0.1, 0.0, -1.0});
     camera->set_viewport_rect(_context->get_viewport());
@@ -34,7 +34,7 @@ void TenthStyleShip::_setup_camera()
 
 void TenthStyleShip::_setup_lights()
 {
-    ZLightRef light = std::make_shared<ZLight>();
+    ZLightRef light = ZLight::create();
     light->set_color(ZColor::white);
     light->set_position(ZVector::zero);
     add_child(light);
@@ -44,19 +44,19 @@ void TenthStyleShip::_setup_models()
 {
     const ZResourceBundle *bundle = ZResourceBundle::get_main_bundle();
     std::string ship_model_path = bundle->get_path_for_resource("TenthStyle.3ds");
-    ZModelRef ship_model = std::make_shared<ZModel>(ship_model_path);
+    ZModelRef ship_model = ZModel::create(ship_model_path);
     
     ZColor material_color(0.25, 0.25, 0.30, 1.0);
-    ZColorMaterialRef material = std::make_shared<ZColorMaterial>();
+    ZColorMaterialRef material = ZColorMaterial::create();
     material->set_color(material_color);
     ship_model->add_material(material);
     
     const float ships_spacing = 12.0;
-    ZNodeRef ships = std::make_shared<ZNode>();
+    ZNodeRef ships = ZNode::create();
     ships->set_position(ZVector::zero);
     
     for (unsigned i = 0; i < SHIPS_COUNT; ++i) {
-        ZNodeRef ship = std::make_shared<ZNode>(ship_model);
+        ZNodeRef ship = ZNode::create(ship_model);
         ship->set_position({0.0, (float)i * ships_spacing, 0.0});
         ships->add_child(ship);
     }
@@ -64,18 +64,18 @@ void TenthStyleShip::_setup_models()
     ZMatrix rotation = ZMatrix::rotation(-M_PI_2, 1.0, 0.0, 0.0);
     ships->set_transform(rotation);
     
-    ZNodeRef ships_container = std::make_shared<ZNode>();
+    ZNodeRef ships_container = ZNode::create();
     ships_container->set_position(ZVector::zero);
     ships_container->add_child(ships);
     
-    ZRotationActionRef rot_action = std::make_shared<ZRotationAction>(2.0 * M_PI, ZVector{0.0, 0.0, 1.0});
+    ZRotationActionRef rot_action = ZRotationAction::create(2.0 * M_PI, ZVector{0.0, 0.0, 1.0});
     rot_action->set_duration(ZTimeInterval(15.0));
     rot_action->set_repeat_count(ZAction::repeat_count_infinity);
     ships_container->add_action(rot_action);
     
     ZVector tr_start = ZVector::zero;
     ZVector tr_end = {0.0, 0.0, ships_spacing};
-    ZTranslationActionRef tr_action = std::make_shared<ZTranslationAction>(tr_start, tr_end);
+    ZTranslationActionRef tr_action = ZTranslationAction::create(tr_start, tr_end);
     tr_action->set_duration(ZTimeInterval(3.0));
     tr_action->set_repeat_count(ZAction::repeat_count_infinity);
     ships_container->add_action(tr_action);
